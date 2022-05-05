8c32ec57943f2fd53652860f278ae019d7e9ee31
Jesús Sánchez Corzo <jesus.sanchezc@codere.com>
2022-05-05 07:12:33 +0200

    Add event in CQRS when Remove Item

--- src/GoalSystem.Inventory.Infrastructure/CQRS/ItemInventory/Commands/RemoveItemInventoryCommandHandler.cs
+++ src/GoalSystem.Inventory.Infrastructure/CQRS/ItemInventory/Commands/RemoveItemInventoryCommandHandler.cs
@@ -1,5 +1,9 @@
 ﻿using GoalSystem.Inventory.Application.Repository;
+using GoalSystem.Inventory.Application.Services;
+using GoalSystem.Inventory.Domain.Enumerations;
+using GoalSystem.Inventory.Domain.Models;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,18 +11,28 @@ namespace GoalSystem.Inventory.Infrastructure.CQRS.ItemInventory.Commands
 {
     public class RemoveItemInventoryCommandHandler : IRequestHandler<RemoveItemInventoryCommand, bool>
     {
+        private readonly ILogger<RemoveItemInventoryCommandHandler> _logger;
         private readonly IRepositoryItemInventory _repositoryItemInventory;
+        private readonly IEventsService _eventsService;
 
-        public RemoveItemInventoryCommandHandler(IRepositoryItemInventory repositoryItemInventory)
+        public RemoveItemInventoryCommandHandler(
+            ILogger<RemoveItemInventoryCommandHandler> logger,
+            IRepositoryItemInventory repositoryItemInventory,
+            IEventsService eventsService)
         {
+            _logger = logger;
             _repositoryItemInventory = repositoryItemInventory;
+            _eventsService = eventsService;
         }
 
         public async Task<bool> Handle(RemoveItemInventoryCommand request, CancellationToken cancellationToken)
         {
             var result = await _repositoryItemInventory.Delete(request.Name);
+            if (!result)
+                return false;
 
-            return result;
+            _logger.LogInformation($"Publish Event when the Remove ItemInventory[{request.Name}]");
+            return await _eventsService.Publish(new Event(TypeEvent.RemoveItemInventory));
         }
     }
 }
